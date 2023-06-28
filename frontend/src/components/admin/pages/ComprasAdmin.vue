5258cfdc1595e7e8f4dc6799609d966d1f535e39
Mayconoliveyra <mayconbrito1998@hotmail.com>
2023-06-28 11:15:15 -0300

    full commit

---
+++ frontend/src/components/admin/pages/ComprasAdmin.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-container fluid class="comprasAdmin">
+  
+  </b-container>
+</template>
+ 
+<script>
+
+export default {
+  name: "CadastroCompras",
+};
+</script>
+
+<style>
+.containerIconeCompras span i {
+  color: rgba(205, 199, 199, 0.592);
+  font-size: 210px;
+}
+</style>
